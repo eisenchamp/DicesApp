948b6b7562f7f18e6283b4b2c9efac5180a743b5
Eisenchamp <ttursynbekov07@gmail.com>
2023-09-21 15:47:48 +0600

    DiceApp Final

--- DicesApp/ViewController.swift
+++ DicesApp/ViewController.swift
@@ -1,19 +1,22 @@
-//
-//  ViewController.swift
-//  DicesApp
-//
-//  Created by Temirlan Tursynbekov on 21.09.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceView1: UIImageView!
+    @IBOutlet weak var diceView2: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [UIImage(named: "dice1"), UIImage(named: "dice2"), UIImage(named: "dice3"), UIImage(named: "dice4"), UIImage(named: "dice5"), UIImage(named: "dice6")]
+        
+        diceView1.image = diceArray[Int.random(in: 0...5)]
+        diceView2.image = diceArray[Int.random(in: 0...5)]
+    }
 }
 
